e2ef9fe213dc02a0aaef63fdd7d72667d6aa1e67
Jekmix <minecrafterm67@gmail.com>
2024-04-15 17:23:48 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Menu from './components/menu/Menu';
 import Delivery from './components/delivery/Delivery';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+      <Navbar />
       <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/menu' element={<Menu/>}></Route>
-          <Route path='/delivery' element={<Delivery/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/menu' element={<Menu />}></Route>
+        <Route path='/delivery' element={<Delivery />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
-      
+      <Footer />
     </div>
   );
 }
